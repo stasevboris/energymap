89fd22ca7cf8a9399bb5e455497658aa3ea57f43
Boris <prostasev@gmail.com>
2026-02-08 23:34:15 +0300

    Внёс изменение с ноута, для проверки работы докера

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Привет, мир!</h1><p>Это тестовая страница EnergyMap в Docker.</p>")
+    return HttpResponse("<h1>Привет, мир!</h1><p>Это тестовая страница EnergyMap в Docker. Внёс изменения с ноута</p>")
